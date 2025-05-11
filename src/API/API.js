8631f17c348df3b9bf2ae1424d2ad94bb099842e
xperalde <danilkakash@mail.ru>
2025-05-12 02:23:22 +0300

    .header{

--- src/API/API.js
+++ src/API/API.js
@@ -1,22 +1,71 @@
 const STORAGE_KEY = 'ne-ugadaesh';
 
+const mockProducts = [
+  {
+    image: "./Image/Vstraiv.svg",
+    name: "Встраиваемый светильник Markt",
+    price: 3490,
+    hasDiscount: true,
+    discountPrice: 5060
+  },
+  {
+    image: "./Image/Lin.svg",
+    name: "Линейный светильник ARG",
+    price: 3490,
+    hasDiscount: false,
+    discountPrice: 0
+  },
+  {
+    image: "./Image/Svetod.svg",
+    name: "Светодиодный светильник",
+    price: 5060,
+    hasDiscount: true,
+    discountPrice: 6060
+  },
+  {
+    image: "./Image/Vstraiv.svg",
+    name: "Встраиваемый светильник Markt",
+    price: 3490,
+    hasDiscount: false,
+    discountPrice: 0
+  },
+  {
+    image: "./Image/Lin.svg",
+    name: "Линейный светильник ARG",
+    price: 6700,
+    hasDiscount: true,
+    discountPrice: 7060
+  },
+  {
+    image: "./Image/Svetod.svg",
+    name: "Светодиодный светильник",
+    price: 5060,
+    hasDiscount: false,
+    discountPrice: 0
+  },
+  {
+    image: "./Image/Vstraiv.svg",
+    name: "Встраиваемый светильник Markt",
+    price: 3490,
+    hasDiscount: true,
+    discountPrice: 7060
+  },
+  {
+    image: "./Image/Lin.svg",
+    name: "Линейный светильник ARG",
+    price: 6700,
+    hasDiscount: false,
+    discountPrice: 0
+  }
+];
+
 export async function fetchProducts() {
   const existing = localStorage.getItem(STORAGE_KEY);
-  
+
   if (existing) {
     return JSON.parse(existing);
   }
 
-  const res = await fetch('/products.json');
-  if (!res.ok) {
-    throw new Error(`Ошибка загрузки данных: ${res.status}`);
-  }
-
-  const products = await res.json();
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(products));
-  return products;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(mockProducts));
+  return mockProducts;
 }
-
-// export function clearProductsStorage() {
-//   localStorage.removeItem(STORAGE_KEY);
-// }
